c759fe3f8ee6e421d1f8f035a29cb110a127eb9e
Carlos Eduardo <112710405+CarlosEduardoSilverio@users.noreply.github.com>
2022-09-28 11:47:34 -0300

    Trabalhos Dia 29.09.2022

--- Dia22_09/estoque.ts
+++ Dia22_09/estoque.ts
@@ -7,13 +7,4 @@ estoque.pop();
 console.log(estoque)
 
 estoque.splice(2,1, "Sofa")
-let estoque: string[] = []
-
-estoque.push("Ferramentas", "Mesas", "Caixas", "Pregos", "Lampadas")
-console.log(estoque);
-
-estoque.pop();
-console.log(estoque)
-
-estoque.splice(2,1, "Sofa")
 console.log(estoque);
