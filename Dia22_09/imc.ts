f128f6c0674b2da23a90c250502102015841b943
Carlos Eduardo <112710405+CarlosEduardoSilverio@users.noreply.github.com>
2022-09-28 11:45:54 -0300

    Trabalhos Dia 29.09.2022

--- Dia22_09/imc.ts
+++ Dia22_09/imc.ts
@@ -1,7 +1,2 @@
 let imc = (altura: number, peso: number) => peso / (altura * altura)  
-
-
-let imc = (altura: number, peso: number) => peso / (altura * altura)  
-
-
 console.log(imc(1.69, 70).toFixed(1))
